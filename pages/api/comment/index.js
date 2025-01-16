e460991d39303f64e8bc10811afa58605706f153
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-01-16 19:00:28 +0530

    Update index.js

--- pages/api/comment/index.js
+++ pages/api/comment/index.js
@@ -11,21 +11,12 @@ const connect = async () => {
 };
 
 // CORS headers
-const setCorsHeaders = (req, res) => {
-  const allowedOrigins = ['http://localhost:3000', 'https://powerhouse-e955.vercel.app'];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  } else {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Use wildcard if you want to allow all origins
-  }
-
+const setCorsHeaders = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'hhttps://powerhouse-e955.vercel.app'); // Update with your front-end URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 };
-
 
 // POST: Add a new comment
 export const addComment = async (req, res) => {
